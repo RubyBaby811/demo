765504b0a02c1fa6f86f269a455b8f1aed31c5f9
ruby0811 <18764209818@163.com>
2021-06-24 17:42:27 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2020-2021, XXX有限公司
+ * FileName: HelloController
+ * Author:   ruby
+ * Date:     2021/6/21 19:56
+ * Description: HelloController
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈HelloController〉
+ *
+ * @author ruby
+ * @create 2021/6/21
+ * @since 1.0.0
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("/index")
+    public String sayHello() {
+        return "index";
+    }
+
+}
